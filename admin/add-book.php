b3231db48cabdc3b1ac70c2aaa3855d58dfbf9a6
Luiso9 <muhandreop@gmail.com>
2024-08-09 17:25:49 +0700

    Sip👍

--- admin/add-book.php
+++ admin/add-book.php
@@ -12,13 +12,21 @@ if (strlen($_SESSION['alogin']) == 0) {
         $author = $_POST['author'];
         $isbn = $_POST['isbn'];
         $price = $_POST['price'];
-        $sql = "INSERT INTO  tblbooks(BookName,CatId,AuthorId,ISBNNumber,BookPrice) VALUES(:bookname,:category,:author,:isbn,:price)";
+
+        // Handle file upload
+        $bookcover = $_FILES['bookcover']['name'];
+        $target_dir = "bookcovers/";
+        $target_file = $target_dir . basename($_FILES["bookcover"]["name"]);
+        move_uploaded_file($_FILES["bookcover"]["tmp_name"], $target_file);
+
+        $sql = "INSERT INTO tblbooks(BookName, CatId, AuthorId, ISBNNumber, BookPrice, BookCover) VALUES(:bookname, :category, :author, :isbn, :price, :bookcover)";
         $query = $dbh->prepare($sql);
         $query->bindParam(':bookname', $bookname, PDO::PARAM_STR);
         $query->bindParam(':category', $category, PDO::PARAM_STR);
         $query->bindParam(':author', $author, PDO::PARAM_STR);
         $query->bindParam(':isbn', $isbn, PDO::PARAM_STR);
         $query->bindParam(':price', $price, PDO::PARAM_STR);
+        $query->bindParam(':bookcover', $bookcover, PDO::PARAM_STR);
         $query->execute();
         $lastInsertId = $dbh->lastInsertId();
         if ($lastInsertId) {
@@ -28,7 +36,6 @@ if (strlen($_SESSION['alogin']) == 0) {
             $_SESSION['error'] = "Something went wrong. Please try again";
             header('location:manage-books.php');
         }
-
     }
     ?>
     <!DOCTYPE html>
@@ -49,103 +56,99 @@ if (strlen($_SESSION['alogin']) == 0) {
         <link href="assets/css/style.css" rel="stylesheet" />
         <!-- GOOGLE FONT -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
     </head>
 
     <body>
         <!------MENU SECTION START-->
         <?php include ('includes/header.php'); ?>
         <!-- MENU SECTION END-->
-        <div class="content-wra
-    <div class=" content-wrapper">
+        <div class="content-wrapper">
             <div class="container">
                 <div class="row pad-botm">
                     <div class="col-md-12">
                         <h4 class="header-line">Add Book</h4>
-
                     </div>
-
                 </div>
                 <div class="row">
-                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3"">
-<div class=" panel panel-info">
-                        <div class="panel-heading">
-                            Book Info
-                        </div>
-                        <div class="panel-body">
-                            <form role="form" method="post">
-                                <div class="form-group">
-                                    <label>Book Name<span style="color:red;">*</span></label>
-                                    <input class="form-control" type="text" name="bookname" autocomplete="off" required />
-                                </div>
+                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                Book Info
+                            </div>
+                            <div class="panel-body">
+                                <form role="form" method="post" enctype="multipart/form-data">
+                                    <div class="form-group">
+                                        <label>Book Name<span style="color:red;">*</span></label>
+                                        <input class="form-control" type="text" name="bookname" autocomplete="off"
+                                            required />
+                                    </div>
 
-                                <div class="form-group">
-                                    <label> Category<span style="color:red;">*</span></label>
-                                    <select class="form-control" name="category" required="required">
-                                        <option value=""> Select Category</option>
-                                        <?php
-                                        $status = 1;
-                                        $sql = "SELECT * from  tblcategory where Status=:status";
-                                        $query = $dbh->prepare($sql);
-                                        $query->bindParam(':status', $status, PDO::PARAM_STR);
-                                        $query->execute();
-                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                        $cnt = 1;
-                                        if ($query->rowCount() > 0) {
-                                            foreach ($results as $result) { ?>
-                                                <option value="<?php echo htmlentities($result->id); ?>">
-                                                    <?php echo htmlentities($result->CategoryName); ?>
-                                                </option>
-                                            <?php }
-                                        } ?>
-                                    </select>
-                                </div>
+                                    <div class="form-group">
+                                        <label> Category<span style="color:red;">*</span></label>
+                                        <select class="form-control" name="category" required="required">
+                                            <option value=""> Select Category</option>
+                                            <?php
+                                            $status = 1;
+                                            $sql = "SELECT * from tblcategory where Status=:status";
+                                            $query = $dbh->prepare($sql);
+                                            $query->bindParam(':status', $status, PDO::PARAM_STR);
+                                            $query->execute();
+                                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                            if ($query->rowCount() > 0) {
+                                                foreach ($results as $result) { ?>
+                                                    <option value="<?php echo htmlentities($result->id); ?>">
+                                                        <?php echo htmlentities($result->CategoryName); ?>
+                                                    </option>
+                                                <?php }
+                                            } ?>
+                                        </select>
+                                    </div>
 
+                                    <div class="form-group">
+                                        <label> Author<span style="color:red;">*</span></label>
+                                        <select class="form-control" name="author" required="required">
+                                            <option value=""> Select Author</option>
+                                            <?php
+                                            $sql = "SELECT * from tblauthors";
+                                            $query = $dbh->prepare($sql);
+                                            $query->execute();
+                                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                            if ($query->rowCount() > 0) {
+                                                foreach ($results as $result) { ?>
+                                                    <option value="<?php echo htmlentities($result->id); ?>">
+                                                        <?php echo htmlentities($result->AuthorName); ?>
+                                                    </option>
+                                                <?php }
+                                            } ?>
+                                        </select>
+                                    </div>
 
-                                <div class="form-group">
-                                    <label> Author<span style="color:red;">*</span></label>
-                                    <select class="form-control" name="author" required="required">
-                                        <option value=""> Select Author</option>
-                                        <?php
+                                    <div class="form-group">
+                                        <label>ISBN Number<span style="color:red;">*</span></label>
+                                        <input class="form-control" type="text" name="isbn" required="required"
+                                            autocomplete="off" />
+                                        <p class="help-block">An ISBN is an International Standard Book Number.ISBN Must be
+                                            unique</p>
+                                    </div>
 
-                                        $sql = "SELECT * from  tblauthors ";
-                                        $query = $dbh->prepare($sql);
-                                        $query->execute();
-                                        $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                        $cnt = 1;
-                                        if ($query->rowCount() > 0) {
-                                            foreach ($results as $result) { ?>
-                                                <option value="<?php echo htmlentities($result->id); ?>">
-                                                    <?php echo htmlentities($result->AuthorName); ?>
-                                                </option>
-                                            <?php }
-                                        } ?>
-                                    </select>
-                                </div>
+                                    <div class="form-group">
+                                        <label>Price<span style="color:red;">*</span></label>
+                                        <input class="form-control" type="text" name="price" autocomplete="off"
+                                            required="required" />
+                                    </div>
 
-                                <div class="form-group">
-                                    <label>ISBN Number<span style="color:red;">*</span></label>
-                                    <input class="form-control" type="text" name="isbn" required="required"
-                                        autocomplete="off" />
-                                    <p class="help-block">An ISBN is an International Standard Book Number.ISBN Must be
-                                        unique</p>
-                                </div>
+                                    <div class="form-group">
+                                        <label>Book Cover Image<span style="color:red;">*</span></label>
+                                        <input class="form-control" type="file" name="bookcover" required="required" />
+                                    </div>
 
-                                <div class="form-group">
-                                    <label>Price<span style="color:red;">*</span></label>
-                                    <input class="form-control" type="text" name="price" autocomplete="off"
-                                        required="required" />
-                                </div>
-                                <button type="submit" name="add" class="btn btn-info">Add </button>
-
-                            </form>
+                                    <button type="submit" name="add" class="btn btn-info">Add </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
-        </div>
         </div>
         <!-- CONTENT-WRAPPER SECTION END-->
         <?php include ('includes/footer.php'); ?>
--- dashboard.php
+++ dashboard.php
@@ -17,6 +17,8 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@1.6.0/dist/flowbite.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/flowbite@1.6.0/dist/flowbite.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap"
+      rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/css/pagedone.css" />
     <script src="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/js/pagedone.js"></script>
     <script>
@@ -60,7 +62,7 @@ if (strlen($_SESSION['login']) == 0) {
             <div class="flex flex-wrap -m-4 text-center">
               <div class="p-4 sm:w-1/4 w-1/2">
                 <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
-                <?php
+                  <?php
                   $sid = $_SESSION['stdid'];
                   $sql1 = "SELECT id from tblissuedbookdetails where StudentID=:sid";
                   $query1 = $dbh->prepare($sql1);
@@ -123,12 +125,12 @@ if (strlen($_SESSION['login']) == 0) {
         </section>
 
         <!-- Blog post, untuk memberikan opsi buku yang baru saja ditambahkan -->
-        <section class="text-gray-600 body-font">
+        <section class="text-gray-600 body-font" style="font-family:'Manrope', sans-serif">
           <div class="container px-5 py-24 mx-auto">
-            <h4 class="header-line text-md font-bold mb-5">BUKU BARU SAJA DITAMBAHKAN</h4>
+            <h4 class="header-line text-md font-bold mb-5">BUKU YANG BARU SAJA DITAMBAHKAN</h4>
             <div class="flex flex-wrap -m-4">
               <?php
-              $sql = "SELECT tblbooks.BookName, tblcategory.CategoryName, tblauthors.AuthorName, tblbooks.BookPrice, tblbooks.RegDate 
+              $sql = "SELECT tblbooks.BookName, tblcategory.CategoryName, tblauthors.AuthorName, tblbooks.BookPrice, tblbooks.RegDate, tblbooks.BookCover
               FROM tblbooks 
               JOIN tblcategory ON tblbooks.CatId = tblcategory.id 
               JOIN tblauthors ON tblbooks.AuthorId = tblauthors.id 
@@ -139,32 +141,31 @@ if (strlen($_SESSION['login']) == 0) {
               if ($query->rowCount() > 0) {
                 foreach ($results as $result) { ?>
                   <div class="p-4 lg:w-1/3 group">
-                    <div
-                      class="transform h-full bg-indigo-400 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative transition duration-500 hover:scale-125 hover:z-10">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-white mb-1">
-                        <?php echo htmlentities($result->CategoryName); ?>
-                      </h2>
-                      <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                        <?php echo htmlentities($result->BookName); ?>
-                      </h1>
-                      <p class="leading-relaxed mb-3"><?php echo htmlentities($result->BookDescription); ?></p>
-                      <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-                        <span
-                          class="text-gray-700 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                          <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" viewBox="0 0 24 24">
-                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                            <circle cx="12" cy="12" r="3"></circle>
-                          </svg><?php echo htmlentities($result->AuthorName); ?>
-                        </span>
-                        <span class="text-gray-900 inline-flex items-center leading-none text-sm">
-                          <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" viewBox="0 0 24 24">
-                            <path
-                              d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                            </path>
-                          </svg><?php echo date('d M Y', strtotime($result->RegDate)); ?>
-                        </span>
+                    <div class="group h-96 w-64 [perspective:1000px] mx-auto">
+                      <div
+                        class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                        <div class="absolute inset-0">
+                          <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40 blur-md"
+                            src="https://picsum.photos/id/870/200/300?grayscale&blur=2?random=2"
+                            alt="<?php echo htmlentities($result->BookName); ?>" />
+                        </div>
+                        <div
+                          class="absolute p-4 inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                          <div class="flex flex-col justify-around gap-6">
+                            <div>
+                              <h1 class="text-xl font-bold"><?php echo htmlentities($result->BookName); ?></h1>
+                              <h2 class="text-sm font-semibold"><?php echo htmlentities($result->CategoryName); ?></h2>
+                            </div>
+                            <div>
+                              <p class="text-sm font-semibold text-left">Author:</p>
+                              <ul class="list-none">
+                                <li class="text-xs text-left"><?php echo htmlentities($result->AuthorName); ?></li>
+                              </ul>
+                            </div>
+                            <button class="w-full mt-2 rounded-xl bg-neutral-800 px-2 py-1 text-sm hover:bg-neutral-900/60"><a
+                                href="#" target="_blank">More Info</a></button>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -174,13 +175,8 @@ if (strlen($_SESSION['login']) == 0) {
           </div>
         </section>
 
-
       </div>
     </div>
-
-
-
-
 
     <!-- CONTENT-WRAPPER SECTION END-->
     <?php include ('includes/footer.php'); ?>
